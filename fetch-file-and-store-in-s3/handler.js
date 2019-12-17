e839ff72371480c41ae463d21a26d041db2c4314
Indunil1992 <indunil@adroitlogic.com>
2019-12-17 14:25:08 +0530

    1

--- fetch-file-and-store-in-s3/handler.js
+++ fetch-file-and-store-in-s3/handler.js
@@ -6,20 +6,20 @@ const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-depe
 const s3 = new AWS.S3();
 
 module.exports.save = (event, context, callback) => {
-  fetch('https://drive.google.com/open?id=1qUQdLIvOW_SdYl_64afr09mX5lSo99eL')
+  fetch(event.image_url)
     .then((response) => {
       if (response.ok) {
         return response;
       }
       return Promise.reject(new Error(
-        `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`));
+            `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`));
     })
     .then(response => response.buffer())
     .then(buffer => (
       s3.putObject({
-        "Body": buffer,
-        "Bucket": "indunil.trigger",
-        "Key": "10"
+        Bucket: process.env.BUCKET,
+        Key: event.key,
+        Body: buffer,
       }).promise()
     ))
     .then(v => callback(null, v), callback);
